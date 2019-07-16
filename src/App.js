26fa5b58edcc732aab13cbeedb967b255a8f85be
jnanendranagara <jvijayakumarostb1.com>
2019-07-16 09:31:35 -0700

    modification

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router,Route,Switch,Redirect } from 'react-router-dom';
 import './App.css';
-import HomePage from './Component/HomePage';
-import SignUp from './Component/SignUp';
+// import HomePage from './Component/HomePage';
+// import SignUp from './Component/SignUp';
 import Home from './Component/Home';
 import Signup1 from './Component/Signup1';
 import SignIn from './Component/SignIn';
@@ -13,8 +13,8 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-           <Route exact path="/first" component={HomePage}></Route>
-           <Route exact path="/create" component={SignUp}></Route>  
+           {/* <Route exact path="/first" component={HomePage}></Route> */}
+           {/* <Route exact path="/create" component={SignUp}></Route>   */}
            <Route exact path="/home" component={Home}></Route> 
            <Route exact path="/signup" component={Signup1}></Route>  
            <Route exact path="/signin" component={SignIn}></Route>   
--- src/Component/Signup1.js
+++ src/Component/Signup1.js
@@ -10,8 +10,8 @@ import browserHistory from "../Utils/browserHistory"
 class Signup1 extends Component{
     constructor(props){
         super(props);
-        this.state={ firstname:'',lastname:'',email:'',password:'', conformpassword:'',
-        firstnameA:'',lastnameA:'',email:'',pwd:'',array:[],
+        this.state={ firstname:'',lastname:'',email:'',password:'',
+        firstnameA:'',lastnameA:'',mail:'',pwd:'',array:[],
        
         }
     }
@@ -20,6 +20,47 @@ class Signup1 extends Component{
     }
     handleSubmit=(event)=>{
         browserHistory.push("/signin")
+    }
+    handleChange=(event)=>{
+        this.setState({[event.target.name]:event.target.value});
+    }
+    handleSubmit=(event)=>{
+        debugger;
+        
+        let j=0;
+        let reqobj={
+            firstname1:this.state.firstname,
+            lastname1:this.state.lastname,
+            emaill:this.state.email,
+            password1:this.state.password 
+        }
+        let ary=this.state.array.push(reqobj)
+        this.setState({ary});
+        let fnamel=this.state.firstname.length, lnamel=this.state.lastname.length, emaill=this.state.email.length, pwdl=this.state.password.length;
+        let reg_firstnameA=/^[A-Za-z]{5,10}$/;
+        let reg_lastnameA=/^[A-Za-z]{5,10}$/;
+        let reg_mail= /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        let reg_pwd=/^[@#][A-Za-z0-9]$/;
+       
+        if(fnamel===0) this.setState({ firstnameA:'Firstname must required'});
+        else if(!reg_firstnameA.test(this.state.firstname)) this.setState({ firstnameA:'Invalid Firstname'});
+        else j++; 
+        
+        if(lnamel===0) this.setState({lastnameA:'Lastname must required'});
+        else if(!reg_lastnameA.test(this.state.lastname)) this.setState({lastnameA:'Invalid Lastname'}); 
+        else j++;
+
+        if(emaill===0) this.setState({user:'Email must required'});
+        else if(!reg_mail.test(this.state.email)) this.setState({mail:'Invalid Email'});
+        else j++;
+
+        if(pwdl===0) this.setState({pwd:'Password must required'});
+        else if(!reg_pwd.test(this.state.password)) this.setState({pwd:'Invalid Password'});
+        else j++;
+
+        if(j>3) {
+            browserHistory.push("home");
+        }
     }
     render(){
         return(
@@ -47,16 +88,20 @@ class Signup1 extends Component{
                       <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
                           <h2 className="signuptext">Sign Up</h2>
                           <label className="label">First name</label><br/>
-                          <input className="input_box " placeholder="Last Name.." name='firstname'></input><br/>
+                          <input className="input_box " placeholder="Last Name.." type="text" name='firstname' onChange={this.handleChange}></input><br/>
+                          <p><b>{this.state.firstnameA}</b></p>
                           <label className="label">Last Name</label><br/>
-                          <input className="input_box " name='lastname' placeholder="Last name.."></input><br/>
+                          <input className="input_box " placeholder="Last name." type="text" name='lastname' onChange={this.handleChange}></input><br/>
+                          <p><b>{this.state.lastnameA}</b></p>
                           <label className="label">Email</label><br/>
-                          <input className="input_box " name='email' placeholder="email."></input><br/>
+                          <input className="input_box " placeholder="email." type="text" name='email' onChange={this.handleChange}></input><br/>
+                          <p><b>{this.state.mail}</b></p>
                           <label className="label">Password</label><br/>
-                          <input className="input_box " placeholder="password."></input><br/>
-                          <label className="label">ConformPassword</label><br/>
-                          <input className="input_box " placeholder="conform password."></input><br/>
-                          <button className="submitbutton">Submit</button><label className="allareadyaccount" onClick={this.handleSubmit}>Already have an Account</label>
+                          <input className="input_box " placeholder="password." type="password"  name='password' onChange={this.handleChange}></input><br/>
+                          <p><b>{this.state.pwd}</b></p>
+                          {/* <label className="label">ConformPassword</label><br/>
+                          <input className="input_box " placeholder="conform password."></input><br/> */}
+                          <button className="submitbutton" onClick={this.handleSubmit}>Submit</button><label className="allareadyaccount" onClick={this.handleSubmit}>Already have an Account</label>
                       </div>
                       <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2"></div>
                       </div>
